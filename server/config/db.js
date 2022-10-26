b7ad83a24a1903ac7a7ced8258f43e27972cf4d1
SUBEENKANG <ksb970813@gmail.com>
2022-10-26 13:06:28 -0400

    update db URL to work with mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://127.0.0.1/book_store"
+    //"URI": "mongodb://127.0.0.1/book_store"
+    "URI": "mongodb+srv://subeenkang:UEkWDzNejuj7txYt@mongodbserver.m2ks0fr.mongodb.net/book_store?retryWrites=true&w=majority"
 }
