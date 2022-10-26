b94d12d5e3d26bbebf39c3bfa528d8e66adf8adb
SUBEENKANG <ksb970813@gmail.com>
2022-10-26 12:36:48 -0400

    final commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,26 @@
+/* 
+FileName: app.css
+Name: Subeen Kang
+StudentNumber: 301185495 
+Date: Oct 22, 2022.
+*/
+
+//IIFE -- Immediately Invoked Function Expression
+(function () {
+    function Start() {
+        console.log("App Started...");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+
+        for (button of deleteButtons) {
+            button.addEventListener('click', (event) => {
+                if (!confirm("Are you sure?")) {
+                    event.preventDefault();
+                    window.location.assign('/book-list');
+                }
+            });
+        }
+    }
+
+    window.addEventListener("load", Start);
+})();
